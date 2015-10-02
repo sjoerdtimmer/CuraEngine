73ed4312969d18da29bb2187c39e18bb232527b2
Tim Kuipers <t.kuipers@ultimaker.com>
2015-10-02 15:32:30 +0200

    moved functions from header to GcodeBuffer.cpp

--- src/GcodeBuffer.h
+++ src/GcodeBuffer.h
@@ -8,12 +8,34 @@
 #include <algorithm>
 #include <list>
 #include <iostream>
+#include <cassert>
 
 #include "utils/logoutput.h"
+#include "timeEstimate.h"
 
 namespace cura 
 {
 
+/*!
+ * Block of gcode commands with the estimated time it takes to execute them.
+ * 
+ * A block is supposed to be a single command with the time it takes to execute it, but since some commands do not take time 
+ * the block should consist of a couple of commands which are executed immmediately, 
+ * followed by a single command which takes time (retraction or move).
+ */
+class GcodeBlock : public std::stringstream
+{ // must be declared outside of GcodeBuffer because timeEstimate.h needs a forward declaration to it.
+public:
+    double time; //!< estimation of the time it takes to print the gcode commands present in this block of gcode
+    TimeEstimateCalculator::Block time_block; //!< A representation of a machine move for time estimation. Don't calculate time from the time_block if it already != zero
+    bool finished;
+    GcodeBlock() 
+    : time(0.0) 
+    , finished(false)
+    { 
+        *this << std::fixed;
+    }
+};
     
      
 /*!
@@ -23,28 +45,10 @@ namespace cura
  */
 class GcodeBuffer 
 {
-    /*!
-     * Block of gcode commands with the estimated time it takes to execute them.
-     * 
-     * A block is supposed to be a single command with the time it takes to execute it, but since some commands do not take time 
-     * the block should consist of a couple of commands which are executed immmediately, 
-     * followed by a single command which takes time (retraction or move).
-     */
-    class Block : public std::stringstream
-    {
-    public:
-        double time; // estimation of the time it takes to print the gcode commands present in this block of gcode
-        Block() 
-        : time(0.0) 
-        { 
-            *this << std::fixed;
-        }
-    };
     
     class Insert : public std::stringstream
     {
-        friend class GcodeBuffer;
-        unsigned int pos; // position of the item in GcodeBuffer::gcode_blocks before which to insert gcode of this Insert
+        unsigned int pos; //!< position of the item in GcodeBuffer::gcode_blocks before which to insert gcode of this Insert
     public:
         unsigned int getPos() const 
         { 
@@ -57,19 +61,36 @@ class GcodeBuffer
         }
     };
     
-    std::vector<Block*> gcode_blocks; // the actual buffer (should always contain at least one Block to write to)
+    std::vector<GcodeBlock*> gcode_blocks; // the actual buffer (should always contain at least one Block to write to)
     
-    std::list<Insert> inserts; // comands to be inserted in the gcode when writing it to file/commandSocket
+    TimeEstimateCalculator estimateCalculator;
+    
+    double totalPrintTime; //!< The total print time of the whole print
     
     double total_time; // the total amount of time it takes to print all gcode currently in this buffer
+    
+    std::list<Insert> inserts; // comands to be inserted in the gcode when writing it to file/commandSocket
     
     std::ostream* output_stream;
 public:
     GcodeBuffer()
-    : total_time(0.0)
+    : estimateCalculator(gcode_blocks)
+    , total_time(0.0)
     , output_stream(&std::cout)
     { 
-        gcode_blocks.push_back(new Block());
+        gcode_blocks.push_back(new GcodeBlock());
+    }
+    
+    
+    double getTotalPrintTime()
+    {
+        return totalPrintTime;
+    }
+
+    void resetTotalPrintTime()
+    {
+        totalPrintTime = 0;
+        assert(gcode_blocks.size() == 1); // this should never be called. The gcode_blocks should already be empty due to the last flush of the previous slicing! (The buffer always contains one block)
     }
     
     GcodeBuffer& operator*() 
@@ -90,51 +111,24 @@ public:
         return *this;
     }
     
-    void estimate(double time_estimate)
-    {
-        gcode_blocks.back()->time = time_estimate;
-        total_time += time_estimate;
-        gcode_blocks.push_back(new Block());
-    }
+    /*!
+     * Conclude a block of code by making it a single unit receiving a time estimate
+     */
+    void estimate(double time);
+    
+    /*!
+     * Conclude a block of code by making it a single unit receiving a time estimate
+     */
+    void estimate(TimeEstimateCalculator::Position newPos, double feedrate);
     
     /*!
      * Creates a gcode writable at (at least) \p time before the end of the gcode in buffer, or after the beginning of the gcode in this buffer.
+     * 
+     * \param time The time before/after which to insert gcode
+     * \param before Whether to insert the gcode before the end of the current buffer or after the start of the current buffer.
+     * \return An insert to which gcode can be written.
      */
-    std::stringstream& insert(double time, bool before)
-    {
-        if (before)
-        {
-            double acc_time = 0.0;
-            for (int pos = gcode_blocks.size() - 1; pos >= 0; pos--)
-            {
-                acc_time += gcode_blocks[pos]->time;
-                if (acc_time >= time)
-                {
-                    inserts.emplace_back(static_cast<unsigned int>(pos));
-                    return inserts.back();
-                }
-            }
-            logWarning("Warning: trying to insert gcode before a time larger than the gcode buffer!");
-            inserts.emplace_front(0);
-            return inserts.front();
-        }
-        else 
-        {
-            double acc_time = 0.0;
-            for (int pos = 0; pos < int(gcode_blocks.size()); pos++)
-            {
-                acc_time += gcode_blocks[pos]->time;
-                if (acc_time >= time)
-                {
-                    inserts.emplace_front(static_cast<unsigned int>(pos));
-                    return inserts.front();
-                }
-            }
-            logWarning("Warning: trying to insert gcode after a time larger than the gcode buffer!");
-            inserts.emplace_back(gcode_blocks.size());
-            return inserts.back();
-        }
-    }
+    std::stringstream& insert(double time, bool before);
     
     /*!
      * Flush the content of the buffer, including the inserts.
@@ -142,35 +136,7 @@ public:
      * Write content to the output_stream and
      * reset all gcode, inserts and time estimates in the buffer.
      */
-    void flush()
-    {
-        inserts.sort([](const Insert& a, const Insert& b) -> bool { return a.pos < b.pos; } );
-        for (unsigned int block_idx = 0; block_idx < gcode_blocks.size(); block_idx++)
-        {
-            while ( ! inserts.empty() && block_idx == inserts.front().getPos())
-            { // insert all inserts which should be inserted at this position (in arbitrary order)
-                *output_stream << inserts.front().str();
-                inserts.pop_front();
-            }
-                
-            Block& block = *gcode_blocks[block_idx];
-            *output_stream << block.str();
-        }
-        while ( ! inserts.empty())
-        { // insert all inserts which should be inserted at the end (and all inserts which were accidentally skipped)
-            *output_stream << inserts.front().str();
-            inserts.pop_front();
-        }
-        
-        for (Block* block : gcode_blocks)
-        {
-            delete block;
-        }
-        gcode_blocks.clear();
-        gcode_blocks.push_back(new Block());
-        inserts.clear();
-        total_time = 0.0;
-    }
+    void flush();
 };
 
 
