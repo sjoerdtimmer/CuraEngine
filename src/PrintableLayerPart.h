2507b380db3fc2620a7af86e4adb81ea926b75ab
Tim Kuipers <t.kuipers@ultimaker.com>
2016-03-03 20:40:22 +0100

    abstract printable layer part class

---
+++ src/PrintableLayerPart.h
@@ -0,0 +1,28 @@
+#ifndef PRINTABLE_LAYER_PART_H
+#define PRINTABLE_LAYER_PART_H
+
+#include "utils/polygon.h"
+#include "utils/AABB.h"
+
+namespace cura
+{
+
+/*!
+ * Abstract class for representing a printable part within a layer.
+ * This could be a SliceLayerPart of a mesh, a piece of support, a prime tower, etc.
+ * 
+ * The PrintableLayerPart knows itself how it should generate its paths.
+ */
+class PrintableLayerPart
+{
+public:
+    virtual Polygons& getOutline() = 0;
+    virtual AABB getBoundaryBox() = 0;
+    virtual int64_t getZ() = 0;
+    virtual void writeGCode() = 0;
+};
+
+
+} // namespace cura
+
+#endif // PRINTABLE_LAYER_PART_H
---
+++ src/PrintableMeshLayerPart.h
@@ -0,0 +1,16 @@
+#ifndef PRINTABLE_MESH_LAYER_PART_H
+#define PRINTABLE_MESH_LAYER_PART_H
+
+#include "PrintableLayerPart.h"
+
+namespace cura
+{
+
+class PrintableMeshLayerPart : public PrintableLayerPart
+{
+    
+};
+
+} // namespace cura
+
+#endif // PRINTABLE_MESH_LAYER_PART_H
