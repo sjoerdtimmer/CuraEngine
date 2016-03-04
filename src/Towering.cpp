966af8c76898c7e4eac3c5198f5222c0d2b1958e
Sjoerd Timmer <sjoerd.timmer@gmail.com>
2016-03-04 21:26:24 +0100

    eerste versie towering. nog niet getest. copiled wel

--- src/Towering.cpp
+++ src/Towering.cpp
@@ -1,142 +1,128 @@
 
-#include "MeshGroup.h"
 #include "settings.h"
-#include "sliceDataStorage.h"
-
-
+#include "PrintableLayer.h"
+#include "Towering.h"
 
 namespace cura {
 
-void Towering::processPrintableLayers(std::vector<PrintableLayer> layers)
+    
+/*! determines the best order to print layerparts such that overlapping layerpart are "towered".
+ *  assumes that layers are ordered.
+ */
+void Towering::processPrintableLayers(std::vector<PrintableLayer>& layers)
 {
-//    
-//    // first count the number of layerparts so we know how big the vector of new layers needs to be:
-//    unsigned int numparts = 0;
-//    for (SliceLayer layer : mesh.layers) {
-//        numparts += layer.parts.size();
-//    }
-//    // we store the SliceLayerParts each in their own SliceLayers, because otherwise the z value would be lost
-//    std::vector<SliceLayer> partiallayers;
-//    partiallayers.reserve(numparts);
-//
-//    for (SliceLayer layer : mesh.layers) {
-//        for (SliceLayerPart layerpart : layer.parts) {
-//            // use emplace to create a completely novel SliceLayer object
-//            SliceLayer newlayer = SliceLayer();
-//            //                    partiallayers.emplace(partiallayers.end()).base();
-//            newlayer.printZ = layer.printZ;
-//            newlayer.sliceZ = layer.sliceZ; // probably never used but for good measure copy it anyway
-//            newlayer.parts.push_back(layerpart);
-//            partiallayers.push_back(newlayer);
-//        }
-//    }
-//
-//    // now delete the old layer storage and replace it with the contents of the new list of layers
-//    mesh.layers.clear();
-//    mesh.layers.reserve(numparts); // resize to same size
-//    for (SliceLayer& layer : partiallayers) {
-//        mesh.layers.push_back(layer);
-//    }
-//
-//    // next: reorder layer(part)s for optimal order
-//    std::list<SliceLayer> toprocess; // all layerparts that need to be processed
-//    std::list<SliceLayer> processed; // the ordered list of items in the best order
-//
-//    // initially everything has to be processed
-//    for (SliceLayer& layer : mesh.layers) {
-//        toprocess.push_back(layer);
-//        //        log("new layer has %d outline polygons\n",layer.parts.back().outline.size());
-//    }
-//
-//    //    log("toprocess layer order:\n");
-//    //    for(SliceLayer& layer : toprocess){
-//    //        log("  %d\n",layer.printZ);
-//    //    }
-//    //    
-//
-//    // the first layer is special because it 'sits' on nothing such that it has align to
-//    assert(toprocess.size() >= 1);
-//
-//    processed.push_front(toprocess.front());
-//    int maximum_height_of_processed_layers = toprocess.front().printZ;
-//    toprocess.pop_front(); // weird that this returns void :s
-//
-//
-//
-//    while (toprocess.size() > 0) {
-//        // find the unprocessed layerpart that 
-//        // 1) does not block any other unprocessed layerpart
-//        // 2) future: optimisations are possible, for instance: stop looking when the inspected unprocessed layerpart is more than layerhight above the highest processed layer...
-//        // 3) with maximum overlap with the lastprocessed layerpart
-//        SliceLayer* best_candidate;
-//        double best_candidate_overlap_area = 0;
-//        bool foundone = false;
-//        std::list<SliceLayer>::iterator candidate_iterator; // use an iterator so that we can store it for later to remove the appropriate element
-//        std::list<SliceLayer>::iterator other_iterator;
-//        std::list<SliceLayer>::iterator best_candidate_it;
-//
-//        for (candidate_iterator = toprocess.begin(); candidate_iterator != toprocess.end(); candidate_iterator++) {
-//            SliceLayer& candidate = *candidate_iterator;
-//            // stop early if this candidate is too far away:
-//            if (candidate.printZ > maximum_height_of_processed_layers + meshgroup->getSettingInMicrons("layer_height")) continue;
-//
-//            bool candidate_is_appropriate = true; // i.e. doesn't block anything that is still to be printed later on
-//            for (other_iterator = toprocess.begin(); other_iterator != toprocess.end() && candidate_is_appropriate; other_iterator++) {
-//                SliceLayer& other = *other_iterator;
-//                if (candidate_iterator == other_iterator) continue; // layers don't block themselves
-//
-//                if (layerBlocksOtherLayer(candidate, other)) {
-//                    candidate_is_appropriate = false;
-//                }
-//            }
-//            if (candidate_is_appropriate) {
-//                // this is an applicable candidate !
-//                // calculate overlap with last layer:
-//                assert(candidate.parts.size() == 1);
-//                assert(processed.back().parts.size() == 1);
-//                double overlap = layerPartOverlapArea(candidate.parts.back(), processed.back().parts.back());
-//                if (overlap > best_candidate_overlap_area) {
-//                    foundone = true; // there is probably a better way to do this
-//                    best_candidate = &candidate;
-//                    best_candidate_overlap_area = overlap;
-//                    best_candidate_it = candidate_iterator;
-//                }
-//            }
-//        }
-//        if (foundone) { // if at least one layer was indeed a good next layerpart
-//            processed.push_back(*best_candidate);
-//            toprocess.erase(best_candidate_it);
-//            log("found a good next layerpart at z=%d\n", best_candidate->printZ);
-//        } else { // no layerpart is good enough for me, simply take the next and admit defeat
-//            // (i.e. a travel move will have to be made)
-//            processed.push_back(toprocess.front());
-//            log("no layer matches any more, falling back to z=%d\n", toprocess.front().printZ);
-//            toprocess.pop_front();
-//        }
-//        maximum_height_of_processed_layers = std::max(maximum_height_of_processed_layers, processed.back().printZ);
-//    }
-//
-//
-//
-//    // 'processed' is now a reordering of layers
-//    // todo: make all of this layer hussling more efficient
-//    mesh.layers.clear();
-//    mesh.layers.reserve(numparts); // resize to same size again
-//    //    log("new layer order:\n");
-//    for (SliceLayer& layer : processed) {
-//        //        log("  %d\n",layer.printZ);
-//        mesh.layers.push_back(layer);
-//    }
-//
-//
-//
-//
-//
-//    // this variable is used to truncate processing of layers above a certain 
-//    // index to prevent extra work if some meshes are shorter than others:
-//    mesh.layer_nr_max_filled_layer = numparts;
+//    PrintableLayerPart* last_processed;
+    
 
-        
+    // the first layer is special because it 'sits' on nothing.
+    // All following layers assume that at least one layerpart has been printed 
+    assert(layers.size() >= 1);
+    assert(layers.front().parts.size() >= 1);
+    // TODO: start with the one closest to startpoint
+//     last_processed = &(layers.front().parts.front());
+    int64_t max_generated_z = layers.front().getZ();
+    // TODO: maybe store the highest processed layernumber instead??
+//     last_processed->generatePaths();
+    
+    Point last_extruder_location = Point(0,0);// TODO: figure out what the last extruder location was
+    
+    // TODO: get the layer height from somewhere
+    // TODO: make maximum lookahead configurable
+    int layer_height = 100;//?.getSettingInMicrons('layer_height');
+    while( this->processNextPart(layers,max_generated_z + 3000 + layer_height, last_extruder_location) )
+    {
+	// ...
+    }   
+}
+
+
+bool Towering::processNextPart(std::vector< PrintableLayer >& layers, int64_t max_z, Point last_extruder_location)
+{
+    // look for a candidate next layerpart that have overlap with last extruded point:
+    for( PrintableLayer& candidatelayer : layers )
+    {
+	
+	if( candidatelayer.getZ() > max_z ) break; // don't look too far ahead
+	
+	// look for parts in this candidatelayer
+	for(PrintableLayerPart& candidatepart : candidatelayer.parts)
+	{
+	    if(candidatepart.isGenerated()) continue; // parts that have already been generated cannot be generated again
+	    if(this->layerPartCanBePrintedNext(candidatepart,layers) && candidatepart.getOutline().inside(last_extruder_location))
+	    {
+		candidatepart.generatePaths();
+		return true;
+	    }
+	}
+    }
+    
+    
+    
+    // no good part found: fall back to simply the next
+    log("tower finished, falling back to any other part\n");
+    for(PrintableLayer& candidatelayer : layers)
+    {
+	for(PrintableLayerPart& candidatepart : candidatelayer.parts){
+	    if( ! candidatepart.isGenerated())
+	    {
+		candidatepart.generatePaths();
+		return true;
+	    }
+	}
+    }
+    
+    
+    // if all parts have been generated
+    log("all parts have been generated\n");
+    return false;
+}
+
+
+
+// whether layer1 blocks printing layer2
+bool Towering::partBlocksOtherPart(PrintableLayerPart& part1, PrintableLayerPart& part2)
+{
+    // some quick heuristics to save time:
+    if(part1.getZ() <= part2.getZ()) return false;       // lower layers never block higher layers
+    if(part2.getZ() - part2.getZ() > 3000) return true;  // never print more than 3mm before catching up. 
+    
+    Polygons& outline1 = part1.getOutline();
+    Polygons& outline2 = part2.getOutline();
+    
+    Polygons keepoutarea = outline2.offset(3000+part1.getZ()-part2.getZ(),ClipperLib::jtRound);
+    
+    if(keepoutarea.intersection(outline1).size() > 0)
+    {
+	return true;
+    }
+    return false;  
+}
+
+
+double Towering::layerPartOverlapArea(PrintableLayerPart& part1, PrintableLayerPart& part2)
+{
+    double area = 0;
+    for(PolygonRef polygon : part1.getOutline().intersection(part2.getOutline()))
+    {
+	area += polygon.area();
+    }
+    return area;
+}
+
+
+bool Towering::layerPartCanBePrintedNext(PrintableLayerPart& part, std::vector<PrintableLayer>& layers)
+{
+    for(PrintableLayer& otherlayer : layers)
+    {
+	for(PrintableLayerPart& otherpart : otherlayer.parts)
+	{
+	    if(otherpart.isGenerated()) continue; // already printed parts are never a reason not to print the current candidate
+	    // TODO: make sure that this is a valid way to check if part and otherpart are the same
+	    if(&part == &otherpart) continue;
+	    if(partBlocksOtherPart(part,otherpart)) return false;
+	}
+    }
+    return true;
 }
 
 }
--- src/Towering.h
+++ src/Towering.h
@@ -8,19 +8,60 @@
 #ifndef TOWERING_H
 #define TOWERING_H
 
-#include <vector>
-#include "PrintableLayerPart.h"
+//#include <vector>
+//#include "PrintableLayerPart.h"
 #include "PrintableLayer.h"
-#include "mesh.h"
+//#include "mesh.h"
 
 namespace cura
 {
 
-class Towering{
+class Towering
+{
 public:
-    processPrintableLayers(std::vector<PrintableLayer> layers);
-};
+    /*! Find the best order to process layerpart. This includes finding towers. This function calls generatePaths() exactly once for every part in every layer that is passed in.
+     *  \param layers the list of layers of parts
+     */
+    void processPrintableLayers(std::vector<PrintableLayer>& layers);
     
+private:
+    
+    
+    /*! Calculate the overlap area between two parts. This is no longer used. Was used to estimate the 'fitness' of the next part given the pevious part. Currently we simply check that the last_extruder_location is inside the part.
+     * \param part1 first part
+     * \param part2 second part
+     * 
+     * \return area of the intersection of the parts
+     */
+    double layerPartOverlapArea(PrintableLayerPart& part1, PrintableLayerPart& part2);
+    
+    
+    /*! Determines whether printing part1 now will cause problems with printing part2 later. I.e. whether part2 hangs over or touches part1.
+     * \param part1 part to be printed now
+     * \param part2 part to be printed later
+     * \return whether this order of printing causes problems
+     */
+    bool partBlocksOtherPart(PrintableLayerPart& part1, PrintableLayerPart& part2);
+    
+    
+    /*! Determines whether a part can be printed without obstructing future parts.
+     * \param part the part that is to be printed
+     * \param layers the collection of layers to check for collisions
+     * \return whether this part can be printed now.
+     */
+    bool layerPartCanBePrintedNext(PrintableLayerPart& part,std::vector<PrintableLayer>& layers);
+    
+    
+    /*! From the list of layers, find a layerpart that fits nicely on to of the last extruded location.
+     * \param layers list of PrintableLayers too choose from.
+     * \param max_z pick a layerpart that is not above this value. used for speeding up the process.
+     * \param last_extruder_location location of the extruder.
+     * 
+     * \return boolean, whether a part was found
+     */
+    bool processNextPart(std::vector< PrintableLayer >& layers, int64_t max_z, Point last_extruder_location);
+};
+
 }
 
 #endif /* TOWERING_H */
